993042f2c2254678305e0357f62daea45fca2202
pavlix <pavel.kraus01@gmail.com>
2019-05-19 12:59:08 +0200

    tree

--- Board.py
+++ Board.py
@@ -1,3 +1,4 @@
+from Tree import Tree
 import random
 
 
@@ -14,17 +15,38 @@ class Board:
                 self.board[i].append(random.randint(1, 6))
 
     def print(self):
-        cellSize = 4  #even numbers only
+        cellSize = 4  # even numbers only
 
         for line in self.board:
-            print('\n' + ('-'*cellSize + '-') * self.boardSize + '-')
+            print('\n' + ('-' * cellSize + '-') * self.boardSize + '-')
 
             for element in line:
-                elementStr = str(2**element)
-                leftSpaces = ' '*((cellSize-len(elementStr))//2)
-                rightSpaces = ' '*(((cellSize-len(elementStr))//2)+len(elementStr) % 2)
+                elementStr = str(2 ** element)
+                leftSpaces = ' ' * ((cellSize - len(elementStr)) // 2)
+                rightSpaces = ' ' * (((cellSize - len(elementStr)) // 2) + len(elementStr) % 2)
                 print('|' + leftSpaces + elementStr + rightSpaces, end='')
 
             print('|', end='')
 
-        print('\n' + ('-'*cellSize + '-') * self.boardSize + '-')
+        print('\n' + ('-' * cellSize + '-') * self.boardSize + '-')
+
+    def findBestPathForCell(self, cell):
+        tree = Tree()
+
+        self.addNodes(tree, self.board[cell[0]][cell[1]], cell)
+
+    def addNodes(self, tree, value, address):
+        currentValue = self.board[address[0]][address[1]]
+
+        if not 0 <= address[0] < self.boardSize:
+            return None
+        if not 0 <= address[1] < self.boardSize:
+            return None
+
+        if currentValue is not value:
+            return None
+
+        tree.value = currentValue
+        tree.address = address
+
+        tree.top = self.addNodes()
---
+++ Tree.py
@@ -0,0 +1,13 @@
+class Tree:
+    parent = None
+    address = (0, 0)
+    value = 0
+
+    left = None
+    right = None
+    top = None
+    bottom = None
+    leftTop = None
+    rightTop = None
+    leftBottom = None
+    rightBottom = None
